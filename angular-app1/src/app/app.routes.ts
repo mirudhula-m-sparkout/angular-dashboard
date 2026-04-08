3a2886e47578c532a870ec4fe68c9658bca1b4b1
Mirudhula <mirudhula864@gmail.com>
2026-04-08 18:43:32 +0530

    Updated

--- angular-app1/src/app/app.routes.ts
+++ angular-app1/src/app/app.routes.ts
@@ -9,10 +9,10 @@ import { SubA1 } from './components/sub-a1/sub-a1';
 import { SubA2 } from './components/sub-a2/sub-a2';
 
 import { ReplaySubject2 } from './components1/replay-subject2/replay-subject2';
+import { Asyncsubcom } from './asyncsubject/asyncsubcom/asyncsubcom';
 
 export const routes: Routes = [
   { path: '', component: Home },
-
   {
     path: 'notifications',
     component: NotifyButtons,
@@ -30,6 +30,15 @@ export const routes: Routes = [
     ]
   },
   {path : 'replaysubject2', component:ReplaySubject2},
+  {path : 'asyncsubcom', component:Asyncsubcom},
+
+  {
+    path: 'rxjsoperations',
+    loadChildren: () =>
+      import('./RxJS-operations/RxJS-operations.routes')
+        .then(m => m.routes)
+  },
+  
   { path: '**', redirectTo: '' }
 ];
 
