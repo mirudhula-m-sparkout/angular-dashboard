50a025b80776d7c38bc825adedd043255cc86bf6
mirudhula-m-sparkout <mirudhula@sparkouttech.com>
2026-04-20 19:00:37 +0530

    Update authservice.ts

--- scholor-flow/src/app/services/authservice.ts
+++ scholor-flow/src/app/services/authservice.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class AuthService { 
 
   constructor(private router: Router) {}
 
